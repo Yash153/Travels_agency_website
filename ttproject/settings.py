a6f53610895565f4a9c1b55e5923a50db6d4b58b
Yash153 <78364754+Yash153@users.noreply.github.com>
2022-01-09 10:46:52 +0530

    Update settings.py

--- ttproject/settings.py
+++ ttproject/settings.py
@@ -126,12 +126,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'demoo132001@gmail.com'
-EMAIL_HOST_PASSWORD = 'ztaobfsxjpmckwpd'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
-razorpay_id = "rzp_test_6HkkoA80aVOiUQ"
-razorpay_account_id = "KFgiBmHPWGAUwBp3mRTDVwOL"
+razorpay_id = ""
+razorpay_account_id = ""
